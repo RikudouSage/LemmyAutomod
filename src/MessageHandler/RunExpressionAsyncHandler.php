5f54f6f004aa59fee69b1d6825284b90d747f345
Rikudou_Sage <dominik@chrastecky.cz>
2025-03-31 20:34:25 +0200

    Feat: Make notifications in async expressions work (#30)

--- src/MessageHandler/RunExpressionAsyncHandler.php
+++ src/MessageHandler/RunExpressionAsyncHandler.php
@@ -2,8 +2,11 @@
 
 namespace App\MessageHandler;
 
+use App\Automod\ModAction\Notification\NotifyOfActionTaken;
+use App\Context\Context;
 use App\Message\RunExpressionAsyncMessage;
 use App\Service\Expression\ExpressionLanguage;
+use App\Service\Expression\ExpressionLanguageNotifier;
 use Symfony\Component\Messenger\Attribute\AsMessageHandler;
 
 #[AsMessageHandler]
@@ -11,11 +14,18 @@ final readonly class RunExpressionAsyncHandler
 {
     public function __construct(
         private ExpressionLanguage $expressionLanguage,
+        private ExpressionLanguageNotifier $notifier,
+        private NotifyOfActionTaken $notifyAction,
     ) {
     }
 
     public function __invoke(RunExpressionAsyncMessage $message): void
     {
         $this->expressionLanguage->evaluate($message->expression, $message->context);
+        if ($this->notifyAction->shouldRun($message->context['object'])) {
+            $this->notifyAction->takeAction($message->context['object'], $this->notifier->currentContext ?? new Context());
+            // to avoid double messages in case it's running synchronously (e.g. when debugging)
+            $this->notifier->currentContext = null;
+        }
     }
 }
