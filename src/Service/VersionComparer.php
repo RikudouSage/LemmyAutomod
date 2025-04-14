5d93c660c3d0fbc747606d36d4751afc130775dd
Rikudou_Sage <dominik@chrastecky.cz>
2025-04-14 22:46:31 +0200

    Feat: Add new version checker (#40)

---
+++ src/Service/VersionComparer.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Service;
+
+final readonly class VersionComparer
+{
+    public function compare(string $left, string $right): int
+    {
+        if ($left === 'dev' && $right === 'dev') {
+            return 0;
+        }
+        if ($left === 'dev') {
+            return -1;
+        }
+        if ($right === 'dev') {
+            return 1;
+        }
+
+        return version_compare($left, $right);
+    }
+}
